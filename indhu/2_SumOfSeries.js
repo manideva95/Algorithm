a6f3432271dce4096a5a39c0ce34236d9fe55b63
Indhu <bkavi1218@gmail.com>
2023-07-20 21:42:52 +0530

    safe commit

---
+++ indhu/2_SumOfSeries.js
@@ -0,0 +1,10 @@
+//https://practice.geeksforgeeks.org/problems/sum-of-series2811/1?page=1&difficulty[]=-2&sortBy=submissions
+
+class Solution {
+    seriesSum(n) {
+        // code here
+        console.log('hello')
+    }
+} 
+
+new Solution().seriesSum(1);
