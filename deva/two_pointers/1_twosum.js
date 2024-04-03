13979550e3a95aed914570e825a2d731c390f438
manideva95 <61878008+manideva95@users.noreply.github.com>
2024-04-03 22:59:25 +0530

    update 1 file and create 2 files

--- deva/two_pointers/1_twosum.js
+++ deva/two_pointers/1_twosum.js
@@ -1,24 +1,27 @@
 // https://leetcode.com/problems/two-sum/
 
-var twoSum = function (nums, target) {
-    // console.log(nums)
-    let left = nums[0]
-    let leftIndex = 0
-    let right = nums[nums.length - 1]
-    let rightIndex = nums.length - 1
-    // console.log(left, right)
-    // console.log(left < right)
+function twoSum(nums, target) {
+    // Sort the array to use two pointers
+    let sortedNums = [...nums].sort((a, b) => a - b);
+    let left = 0;
+    let right = sortedNums.length - 1;
+
     while (left < right) {
-        if ((left + right) == target) {
-            return [left, right]
-        } else if ((left + right) > target) {
-            right = nums[--rightIndex]
-        } else if ((left + right) < target) {
-            left = nums[++leftIndex]
+        let sum = sortedNums[left] + sortedNums[right];
+        if (sum === target) {
+            // Find indices of two numbers in the original array
+            let index1 = nums.indexOf(sortedNums[left]);
+            let index2 = nums.lastIndexOf(sortedNums[right]);
+            return [index1, index2];
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
         }
     }
-    return null
-};
 
+    return [];
+}
 
 console.log(twoSum([2, 7, 11, 15], 9))
+console.log(twoSum([3, 2, 4], 6))
