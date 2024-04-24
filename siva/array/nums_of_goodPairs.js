2e164c246e68929a35bc1723c6355704be1f9401
siva <sivasankaran1598@gmail.com>
2024-04-24 20:40:03 +0530

    good pairs solved

---
+++ siva/array/nums_of_goodPairs.js
@@ -0,0 +1,13 @@
+var numIdenticalPairs = function (nums) {
+  let goodPairs = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] == nums[j]) {
+        goodPairs++;
+      }
+    }
+  }
+  return goodPairs;
+};
+
+console.log(numIdenticalPairs([1, 2, 3, 1, 1, 3]));
