bcfd8f9cc6580c6fa11da0a45d1535d3290be9ba
Indhu <bkavi1218@gmail.com>
2024-05-09 22:05:29 +0530

    string problem

--- indhu/leetcode/1_string/2194.js
+++ indhu/leetcode/1_string/2194.js
@@ -4,21 +4,29 @@ class Solution {
 
     // topic: string
     fn(s) {
-        return
+        let alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        const start = alphabets.indexOf(s[0]);
+        const end = alphabets.indexOf(s[3]);
+        const result = [];
+        for (let i = start; i <= end; i++) {
+            for (let j = parseInt(s[1]); j <= parseInt(s[4]); j++) {
+                result.push(`${alphabets[i]}${j}`)
+            }
+        }
+        return result
     }
 
 }
 
-const data = new Solution().fn('131.12.12.23');
+const data = new Solution().fn('F3:L4');
 console.log(data);
-
-// Observation:
-// vocabulary: defanged
-// input: valid ip
-// output: replace . to [.]
-// Methods: replaceAll() [time complexity : O(n * m)]
-// output Time Complexity : O(n*m)
 
 
 // Alternate
-// address.split(".").join('[.]')  split[O(n)], join[O(n)] => output  O(2n)
+//     let e=[]
+// for(let i=s[0].charCodeAt();i<=s[3].charCodeAt();i++){
+//     for(let j=s[1];j<=s[4];j++){
+//         e.push(String.fromCharCode(i)+j)
+//     }
+// }
+// return e
---
+++ indhu/leetcode/1_string/2942.js
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/find-words-containing-character/
+
+class Solution {
+
+    // topic: string
+    fn(words, s) {
+
+        const result = [];
+        let j = words.length - 1;
+        let i = 0;
+        while (i <= j) {
+            if (i == j) {
+                if (words[i].includes(s)) {
+                    result.push(i)
+                }
+                break
+            }
+            if (words[i].includes(s)) {
+                result.push(i)
+            }
+            if (words[j].includes(s)) {
+                result.push(j)
+            }
+            i++;
+            j--;
+        }
+        return result
+    }
+
+}
+
+const data = new Solution().fn(['sdf', 'dsf', 'erer', 'e', 'de'], 'e');
+console.log(data);
+
+
+// Alternate
+//     let e=[]
+// for(let i=s[0].charCodeAt();i<=s[3].charCodeAt();i++){
+//     for(let j=s[1];j<=s[4];j++){
+//         e.push(String.fromCharCode(i)+j)
+//     }
+// }
+// return e
