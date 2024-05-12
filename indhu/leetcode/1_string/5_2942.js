c4c529a54d84a40db4ab0835a89113039a44c112
Indhu <bkavi1218@gmail.com>
2024-05-12 09:55:23 +0530

    name changes

--- indhu/leetcode/1_string/5_2942.js
+++ indhu/leetcode/1_string/5_2942.js
@@ -35,7 +35,7 @@ console.log(data);
 
 // Alternate
 //     let e=[]
-// for(let i=s[0].charCodeAt();i<=s[3].charCodeAt();i++){
+// for(let i=s[0].charCodeAt();i<=s[3].charCodeAt();i++){ 
 //     for(let j=s[1];j<=s[4];j++){
 //         e.push(String.fromCharCode(i)+j)
 //     }
