fab36bdd8a0bf6cb06f908359de4b3191eab8bf2
UshaNandhini11 <ushaanbazhagan@gmail.com>
2024-10-13 22:51:27 +0530

    Adds 2pointer prob 18

---
+++ usha/leetcode/2_TwoPointer/easy/18_283.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/move-zeroes/description/
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums.length == 1) {
+            return;
+        }
+
+        int nonZeroIndex = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroIndex] = nums[i];
+                nonZeroIndex++;
+            }
+        }
+
+       
+         while(nonZeroIndex<nums.length){
+            nums[nonZeroIndex] = 0;
+            nonZeroIndex++;
+        }
+    }
+}
+
+//time complexity: O(n)
