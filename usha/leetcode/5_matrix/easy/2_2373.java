7379d21253558a1e16ee5863d4dc956be3277902
UshaNandhini11 <ushaanbazhagan@gmail.com>
2025-01-08 22:05:12 +0530

    Adds matrix easy prob

---
+++ usha/leetcode/5_matrix/easy/2_2373.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/largest-local-values-in-a-matrix/description/
+
+// Topics
+// Array
+// Matrix
+
+class Solution {
+    public static void main(String[] arg) {
+        int[][] accounts = {
+//              {1, 2, 3},{3, 2, 1}
+                {1, 5}, {7, 3}, {3, 5}
+        };
+        System.out.println("output:::" + maximumWealth(accounts));
+    }
+    public int[][] largestLocal(int[][] grid) {
+        
+    }
+}
